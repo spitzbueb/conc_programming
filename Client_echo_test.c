b95a0d954dae5982001b615139cd8e753deb2729
Philipp Schalcher <philipp.schalcher@gmail.com>
2014-04-09 17:07:37 +0200

    Netzwerkteil implementiert, Einleitung geschrieben

---
+++ Client_echo_test.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#include <sys/msg.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <unistd.h>
+#include <signal.h>
+#include <time.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main(int argc, char *argv[])
+{
+	int sockfd, portno, n;
+	struct sockaddr_in serv_addr;
+	struct hostent *server;
+	char *servIP;
+	
+	char buffer[256];
+	
+	portno = 6666;
+	servIP="192.168.74.129";
+	
+	sockfd = socket(PF_INET,SOCK_STREAM,IPPROTO_TCP);
+	if(sockfd < 0)
+	{
+		printf("ERROR CREATING SOCKET!\n");
+		exit(1);
+	}
+	
+	memset(&serv_addr,0,sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = inet_addr(servIP);
+	serv_addr.sin_port = htons(portno);
+	
+	n = connect(sockfd,(struct sockaddr *) &serv_addr, sizeof(serv_addr));
+	if(n<0)
+	{
+		printf("ERROR CONNECTING!\n");
+		exit(1);
+	}
+	
+	while(1)
+	{
+		fgets(buffer,256,stdin);
+		
+		n = write(sockfd, buffer, strlen(buffer));
+		
+		if(n<0)
+		{
+			printf("ERROR WRITING TO SOCKET!\n");
+			exit(1);
+		}
+		
+		bzero(buffer,256);
+		
+		n = read(sockfd, buffer, 255);
+		if(n<0)
+		{
+			printf("ERROR READING SOCKET!\n");
+			exit(1);
+		}
+		
+		printf("Message was: %s\n", buffer);
+	}
+	
+	
+}
--- Server.c
+++ Server.c
@@ -12,11 +12,8 @@
 #include <signal.h>
 #include <time.h>
 #include <fcntl.h>
-
-struct datei
-{
-	char *dateiname;
-};
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 key_t key;
 int shmid;
@@ -24,5 +21,76 @@ int shmid;
 
 int main(int argc, char *argv[])
 {
-	struct datei dateien[];
+	
+	int sockfd, clntLen, portno, newsockfd;
+	char buffer[256];
+	struct sockaddr_in server_addr, clnt_addr;
+	int retcode;
+	
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	
+	if (sockfd < 0)
+	{
+		perror("ERROR OPENING SOCKET!\n");
+		exit(1);
+	}
+	
+	bzero((char *) &server_addr, sizeof(server_addr));
+	portno = 6666;
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = INADDR_ANY;
+	server_addr.sin_port = htons(portno);
+	
+	if(bind(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0)
+	{
+		perror("ERROR BINDING!\n");
+		exit(1);
+	}
+	
+	listen(sockfd,100);
+	clntLen = sizeof(clnt_addr);
+	
+	while(1)
+	{
+		newsockfd = accept(sockfd, (struct sockaddr *) &clnt_addr, &clntLen);
+		
+		int pid;
+		
+		pid = fork();
+		
+		if(pid < 0)
+		{
+			close(newsockfd);
+			perror("ERROR ACCEPTING CONNECTION!\n");
+			exit(1);
+		}
+		else if(pid > 0)
+		{
+			continue;
+		}
+		else
+		{
+			/* CHILD PART */
+			while(1)
+			{
+				retcode = read(newsockfd, buffer, 255);
+				if(retcode < 0)
+				{
+					perror("ERROR READING SOCKET!\n");
+					exit(1);
+				}
+			
+				printf("Your Message: %s\n", buffer);
+			
+				retcode = write(newsockfd, buffer, strlen(buffer));
+				if(retcode < 0)
+				{
+					printf("ERROR WRITING SOCKET!\n");
+					exit(1);
+				}
+				
+				printf("DONE!\n");
+			}
+		}
+	}
 }
